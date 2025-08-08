a14846fc0c577dd547012655c634d2386b151e43
jonathan1548 <jonathan.ashraf@yahoo.com>
2025-08-08 14:36:30 +0300

    get search bar focus

--- src/App.js
+++ src/App.js
@@ -153,6 +153,11 @@ function Logo() {
 }
 
 function Search({ query, setQuery }) {
+  useEffect(function(){
+    const el = document.querySelector(".search");
+    el.focus();
+  },[])
+
   return (
     <input
       className="search"
